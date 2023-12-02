ee8ef0afeb37ae522c9047409afb9b47338d4cf0
Robin Michael <Siliconrob@siliconheaven.net>
2023-12-02 13:15:22 -0600

    comprehensions

--- day2.py
+++ day2.py
@@ -45,9 +45,7 @@ class GameBag:
 
 def get_game_result(game_result_text: str) -> GameResult:
     id, turns = parse('Game {:d}: {}', game_result_text)
-    parsed_turns = []
-    for turn_result in turns.split(";"):
-        parsed_turns.append(get_game_turn(turn_result))
+    parsed_turns = [get_game_turn(turn_result) for turn_result in turns.split(";")]
     game = GameResult(id, parsed_turns)
     ic(game)
     return game
@@ -57,31 +55,27 @@ def get_game_turn(game_line: str) -> list[GameTurn]:
     new_turn = GameTurn(0, 0, 0)
     for current_turn in game_line.split(","):
         count, color = parse('{:d} {}', current_turn)
-        if color == 'red':
-            new_turn.red = count
-            continue
-        if color == 'blue':
-            new_turn.blue = count
-            continue
-        if color == 'green':
-            new_turn.green = count
-            continue
+        match color:
+            case 'red':
+                new_turn.red = count
+            case 'blue':
+                new_turn.blue = count
+            case 'green':
+                new_turn.green = count
     return new_turn
 
 
 if __name__ == '__main__':
-    #data = get_data(day=2, year=2023).splitlines()
-    data = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
-            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
-            'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
-            'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
-            'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
+    data = get_data(day=2, year=2023).splitlines()
+    # data = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+    #         'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+    #         'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+    #         'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+    #         'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
 
     valid_games = []
     game_bounds = GameBag(12, 13, 14)
-
     part2 = 0
-
     for current_line in data:
         current_game_results = get_game_result(current_line)
         if game_bounds.is_game_valid(current_game_results):
