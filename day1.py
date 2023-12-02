75b532a5fdba895f74d2fe51c5290789f8f4d0fc
Robin Michael <Siliconrob@siliconheaven.net>
2023-12-01 18:52:40 -0600

    Extract getting number out

--- day1.py
+++ day1.py
@@ -2,66 +2,42 @@ import itertools
 from aocd import get_data
 
 
-def get_part2_extracted_number(input_line: str) -> int:
-    valid_text_numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    first_digit = None
-    last_digit = None
-
-    first_number_positions = {}
+def extract_number_portion(input_line: str, text_numbers: list[str]) -> int:
+    extracted_number = None
+    number_positions = {}
     for current_index, current_character in enumerate(input_line):
         if current_character.isdigit():
-            first_digit = current_character
-            first_number_positions[current_index] = int(current_character)
+            extracted_number = current_character
+            number_positions[current_index] = int(current_character)
             break
-    if first_digit is None or 2 < current_index < len(input_line):
-        if first_digit is None:
+    if extracted_number is None or 2 < current_index < len(input_line):
+        if extracted_number is None:
             current_index = len(input_line)
         partial_line = input_line[:current_index]
         if partial_line.isalpha():
-            for text_number in valid_text_numbers:
+            for text_number in text_numbers:
                 found_index = partial_line.find(text_number)
                 if found_index > -1:
-                    first_number_positions[found_index] = valid_text_numbers.index(text_number) + 1
-            number_keys = list(first_number_positions.keys())
+                    number_positions[found_index] = text_numbers.index(text_number) + 1
+            number_keys = list(number_positions.keys())
             number_keys.sort()
-            first_digit = first_number_positions[number_keys[0]]
+            extracted_number = number_positions[number_keys[0]]
         else:
-            for text_number in valid_text_numbers:
+            for text_number in text_numbers:
                 if text_number in partial_line:
-                    first_digit = valid_text_numbers.index(text_number) + 1
+                    extracted_number = text_numbers.index(text_number) + 1
                     break
+    return extracted_number
 
+
+def get_part2_extracted_number(input_line: str) -> int:
+    valid_text_numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    first_digit = extract_number_portion(input_line, valid_text_numbers)
     reversed_text_numbers = []
     for text_number in valid_text_numbers:
         reversed_text_numbers.append(text_number[::-1])
-
     reversed_input_line = input_line[::-1]
-
-    last_number_positions = {}
-    for current_index, current_character in enumerate(reversed_input_line):
-        if current_character.isdigit():
-            last_digit = current_character
-            last_number_positions[current_index] = int(current_character)
-            break
-
-    if last_digit is None or 2 < current_index < len(reversed_input_line):
-        if last_digit is None:
-            current_index = len(reversed_input_line)
-        partial_line = reversed_input_line[:current_index]
-        if partial_line.isalpha():
-            for text_number in reversed_text_numbers:
-                found_index = partial_line.find(text_number)
-                if found_index > -1:
-                    last_number_positions[found_index] = reversed_text_numbers.index(text_number) + 1
-            number_keys = list(last_number_positions.keys())
-            number_keys.sort()
-            last_digit = last_number_positions[number_keys[0]]
-        else:
-            for text_number in reversed_text_numbers:
-                if text_number in partial_line:
-                    last_digit = reversed_text_numbers.index(text_number) + 1
-                    break
-
+    last_digit = extract_number_portion(reversed_input_line, reversed_text_numbers)
     return int(f'{first_digit}{last_digit}')
 
 
