4b92ef2b32b4f13d3e66d7714efa1e6d85cb0186
Robin Michael <Siliconrob@siliconheaven.net>
2023-12-03 13:27:38 -0600

    Part 1

--- day3.py
+++ day3.py
@@ -3,91 +3,99 @@ from dataclasses import dataclass
 from parse import parse
 from aocd import get_data
 from icecream import ic
+from collections import deque
 
 
-@dataclass
-class GameTurn:
-    blue: int
-    red: int
-    green: int
+def get_symbol_part(check_row_index: int, check_col_index: int, row_data: str, all_rows: []) -> int:
+    if -1 < check_row_index < len(data) and -1 < check_col_index < len(row_data):
+        target_row = all_rows[check_row_index]
+        target_char = target_row[check_col_index]
+        if target_char.isnumeric():
+            return get_number(target_row, check_col_index)
+    return None
 
 
-@dataclass
-class GameResult:
-    id: int
-    turns: list[GameTurn]
+def get_number(row_text: str, start_index: int) -> int:
+    left_side = [*row_text[:start_index]]
+    right_side = deque([*row_text[start_index:]])
 
-    def red_cubes(self):
-        return max(turn.red for turn in self.turns)
+    current_digits = []
+    while left_side:
+        current_element = left_side.pop()
+        if current_element.isnumeric():
+            current_digits.append(current_element)
+        else:
+            break
 
-    def green_cubes(self):
-        return max(turn.green for turn in self.turns)
+    current_digits.reverse()
+    while right_side:
+        current_element = right_side.popleft()
+        if current_element.isnumeric():
+            current_digits.append(current_element)
+        else:
+            break
 
-    def blue_cubes(self):
-        return max(turn.blue for turn in self.turns)
-
-    def min_cubes_to_play_game(self) -> int:
-        ic(f'Red: {self.red_cubes()}, Green: {self.green_cubes()}, Blue: {self.blue_cubes()}')
-        return self.red_cubes() * self.green_cubes() * self.blue_cubes()
-
-@dataclass
-class GameBag:
-    red: int
-    green: int
-    blue: int
-
-    def is_game_valid(self, game: GameResult) -> bool:
-        for game_turn in game.turns:
-            if game_turn.red > self.red or game_turn.blue > self.blue or game_turn.green > self.green:
-                return False
-        return True
+    number_text = ''.join(digit for digit in current_digits)
+    return ic(int(number_text))
 
 
-def get_game_result(game_result_text: str) -> GameResult:
-    id, turns = parse('Game {:d}: {}', game_result_text)
-    parsed_turns = []
-    for turn_result in turns.split(";"):
-        parsed_turns.append(get_game_turn(turn_result))
-    game = GameResult(id, parsed_turns)
-    ic(game)
-    return game
-
-
-def get_game_turn(game_line: str) -> list[GameTurn]:
-    new_turn = GameTurn(0, 0, 0)
-    for current_turn in game_line.split(","):
-        count, color = parse('{:d} {}', current_turn)
-        if color == 'red':
-            new_turn.red = count
-            continue
-        if color == 'blue':
-            new_turn.blue = count
-            continue
-        if color == 'green':
-            new_turn.green = count
-            continue
-    return new_turn
+def get_uniques(input_numbers: dict) -> list[int]:
+    uniques = []
+    for the_number in input_numbers.values():
+        if the_number is not None:
+            uniques.append(the_number)
+    return uniques
 
 
 if __name__ == '__main__':
-    #data = get_data(day=2, year=2023).splitlines()
-    data = ['Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
-            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
-            'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
-            'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
-            'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green']
+    data = get_data(day=3, year=2023).splitlines()
+    # data = ['467..114..',
+    #         '...*......',
+    #         '..35..633.',
+    #         '......#...',
+    #         '617*......',
+    #         '.....+.58.',
+    #         '..592.....',
+    #         '......755.',
+    #         '...$.*....',
+    #         '.664.598..']
 
-    valid_games = []
-    game_bounds = GameBag(12, 13, 14)
+    ic(data)
 
-    part2 = 0
+    part_numbers = []
+    for row_index, row_data in enumerate(data):
+        symbol_parts = []
+        for col_index, col_value in enumerate(row_data):
+            if col_value.isnumeric() or col_value == '.':
+                continue
 
-    for current_line in data:
-        current_game_results = get_game_result(current_line)
-        if game_bounds.is_game_valid(current_game_results):
-            valid_games.append(current_game_results)
-        part2 += current_game_results.min_cubes_to_play_game()
+            top_numbers = {'left': get_symbol_part(row_index - 1, col_index - 1, row_data, data),
+                           'above': get_symbol_part(row_index - 1, col_index, row_data, data),
+                           'right': get_symbol_part(row_index - 1, col_index + 1, row_data, data)}
 
-    part1 = sum(valid_game.id for valid_game in valid_games)
+            if top_numbers['left'] == top_numbers['above']:
+                top_numbers['left'] = None
+            if top_numbers['right'] == top_numbers['above']:
+                top_numbers['right'] = None
+
+            number_positions = {'left': get_symbol_part(row_index, col_index - 1, row_data, data),
+                                'right': get_symbol_part(row_index, col_index + 1, row_data, data)}
+
+            bottom_numbers = {'left': get_symbol_part(row_index + 1, col_index - 1, row_data, data),
+                              'below': get_symbol_part(row_index + 1, col_index, row_data, data),
+                              'right': get_symbol_part(row_index + 1, col_index + 1, row_data, data)}
+
+            if bottom_numbers['left'] == bottom_numbers['below']:
+                bottom_numbers['left'] = None
+            if bottom_numbers['right'] == bottom_numbers['below']:
+                bottom_numbers['right'] = None
+
+            symbol_parts.extend(get_uniques(top_numbers))
+            symbol_parts.extend(get_uniques(number_positions))
+            symbol_parts.extend(get_uniques(bottom_numbers))
+
+        if len(symbol_parts) > 0:
+            part_numbers.extend(symbol_parts)
+
+    part1 = sum(part_numbers)
     ic(f'Part 1: {part1}')
-    ic(f'Part 2: {part2}')
