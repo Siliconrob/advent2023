ebc0b7c7845330bebc47bbf4e049e064f6cc9fc4
Robin Michael <Siliconrob@siliconheaven.net>
2023-12-05 08:28:23 -0600

    switch to dict for mappings because part 2 need to separate

--- day5.py
+++ day5.py
@@ -1,13 +1,8 @@
 import itertools
-import math
-import re
-from dataclasses import dataclass
-from parse import parse
-from aocd import get_data
-from icecream import ic
-from collections import deque
-from itertools import groupby
 from functools import partial
+from itertools import groupby
+from icecream import ic
+from parse import parse
 
 
 def parse_seeds(seed_group: list[str]) -> list[int]:
@@ -25,33 +20,36 @@ def map_translator(start: int, end: int, range: int, input_number: int) -> int:
     return input_number
 
 
-def parse_mappings(mapping_group: list[str]) -> list[partial]:
+def parse_mappings(mapping_group: list[str]) -> (str, list[partial]):
     mapping_fns = []
+    mapping_name = mapping_group[0]
     for mapping_fn in mapping_group[1:]:
         end, start, range = parse('{:d} {:d} {:d}', mapping_fn)
         new_fn = partial(map_translator, start, end, range)
         mapping_fns.append(new_fn)
-    return mapping_fns
+    return mapping_name, mapping_fns
 
 
-def get_mapping_chain_fns(mapping_groupings):
-    fn_chains = []
+def get_mapping_chain_fns(mapping_groupings) -> dict:
+    fn_chains = {}
     for input_group in mapping_groupings:
-        new_mapping_fns = parse_mappings(input_group)
-        fn_chains.append(new_mapping_fns)
+        new_map_name, new_mapping_fns = parse_mappings(input_group)
+        fn_chains[new_map_name] = new_mapping_fns
     return fn_chains
 
 
-def part1_solve(mapping_chains, seeds) -> int:
+def part1_solve(mapping_chains: dict, seeds: list[int]) -> int:
     seed_locations = []
     for seed in seeds:
         current = seed
-        for mapping_chain in mapping_chains:
+        for map_name, mapping_chain in mapping_chains.items():
+            ic(map_name)
             results = ic(
                 list(itertools.filterfalse(lambda x: x is None, [map_fn(current) for map_fn in mapping_chain])))
             current = ic(current if len(results) == 0 else results.pop())
         seed_locations.append(current)
     return min(seed_locations)
+
 
 if __name__ == '__main__':
     # data = get_data(day=5, year=2023).splitlines()
