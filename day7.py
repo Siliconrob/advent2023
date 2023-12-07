a736988559bf1f87b5a4c5f5fa68c57814a0895e
Robin Michael <Siliconrob@siliconheaven.net>
2023-12-07 00:51:02 -0600

    Part 1 done

--- day7.py
+++ day7.py
@@ -1,79 +1,90 @@
-import functools
-import itertools
-import math
+from aocd import get_data
+from collections import Counter, defaultdict
 from dataclasses import dataclass
-
+from parse import parse
 from icecream import ic
 
-
-def run_race(end: int, record: int, start_time: int) -> int:
-    if start_time >= end:
-        return 0
-    travel_distance = (end - start_time) * start_time
-    if travel_distance <= record:
-        return 0
-    return travel_distance
+ranker = str.maketrans("AKQJT98765432", "abcdefghijklm")
 
 
 @dataclass
-class WinningTime:
-    hold_time: int
-    distance: int
+class PlayerAction:
+    hand: str
+    bid_amount: int
 
 
 @dataclass
-class RaceRecord:
-    time: int
-    distance: int
+class RankedGroups:
+    five_of_a_kind = []
+    four_of_a_kind = []
+    full_house = []
+    three_of_a_kind = []
+    two_pair = []
+    one_pair = []
+    high_card = []
+
+    def sort_group_ranking(self):
+        return [*sorted(self.five_of_a_kind, key=lambda x: x.hand.translate(ranker)),
+                *sorted(self.four_of_a_kind, key=lambda x: x.hand.translate(ranker)),
+                *sorted(self.full_house, key=lambda x: x.hand.translate(ranker)),
+                *sorted(self.three_of_a_kind, key=lambda x: x.hand.translate(ranker)),
+                *sorted(self.two_pair, key=lambda x: x.hand.translate(ranker)),
+                *sorted(self.one_pair, key=lambda x: x.hand.translate(ranker)),
+                *sorted(self.high_card, key=lambda x: x.hand.translate(ranker))]
 
 
-def parse_numbers_from_line(input_line: str) -> list[int]:
-    return [int(valid_number) for valid_number in list(
-        itertools.filterfalse(lambda x: x == '', [number for number in input_line.split(":")[1].strip().split(" ")]))]
+def parse_player_action(player_round: str) -> PlayerAction:
+    hand, bid = parse('{} {:d}', player_round)
+    return PlayerAction(hand, bid)
 
 
-def parse_race_records_part1(race_times: str, race_distances: str) -> list[RaceRecord]:
-    times = parse_numbers_from_line(race_times)
-    distances = parse_numbers_from_line(race_distances)
-    return [RaceRecord(race_time, race_distance) for race_time, race_distance in zip(times, distances)]
+def rank_hands(hands: list[PlayerAction]) -> list[PlayerAction]:
+    ranked_groups = RankedGroups()
+    for hand in hands:
+        uniques = Counter(hand.hand)
+        if len(uniques.keys()) == 1:
+            ranked_groups.five_of_a_kind.append(hand)
+            continue
+        if len(uniques.keys()) == 2:
+            top_cards, bottom_cards = uniques.most_common(2)
+            if top_cards[1] == 4:
+                ranked_groups.four_of_a_kind.append(hand)
+            elif top_cards[1] == 3 and bottom_cards[1] == 2:
+                ranked_groups.full_house.append(hand)
+            continue
+        if len(uniques.keys()) == 3 and uniques.most_common(1)[0][1] == 3:
+            ranked_groups.three_of_a_kind.append(hand)
+            continue
+        remaining_ranks = uniques.most_common(5)
+        if len(remaining_ranks) == 5:
+            ranked_groups.high_card.append(hand)
+            continue
+        first_rank = remaining_ranks[0]
+        second_rank = remaining_ranks[1]
+        if first_rank[1] == second_rank[1]:
+            ranked_groups.two_pair.append(hand)
+            continue
+        ranked_groups.one_pair.append(hand)
+    ordered_hands = ic(ranked_groups.sort_group_ranking())
+    return ordered_hands
 
 
 def part1_solve(input_data: list[str]):
-    parsed_race_records = ic(parse_race_records_part1(input_data[0], input_data[1]))
-    race_strategies = {}
-    for race_record in parsed_race_records:
-        current_race_winners = []
-        for hold_time in range(0, race_record.time):
-            new_record = run_race(race_record.time, race_record.distance, hold_time)
-            if new_record > 0:
-                current_race_winners.append(WinningTime(hold_time, new_record))
-        race_strategies[race_record.time] = current_race_winners
-    winning_strategies = []
-    for race, winners in race_strategies.items():
-        winning_strategies.append(len(winners))
-    ic(f'Part 1: {math.prod(winning_strategies)}')
+    current_game = [parse_player_action(input_line) for input_line in input_data]
+    ranked_game = rank_hands(current_game)
 
-
-def part2_solve(input_data: list[str]):
-    race_time = int(functools.reduce(lambda x, y: x + y, itertools.filterfalse(lambda x: x == ' ', [number for number in
-                                                                                                    input_data[0].split(
-                                                                                                        ":")[
-                                                                                                        1].strip()])))
-    race_distance = int(
-        functools.reduce(lambda x, y: x + y, itertools.filterfalse(lambda x: x == ' ', [number for number in
-                                                                                        input_data[1].split(
-                                                                                            ":")[1].strip()])))
-    big_race = ic(RaceRecord(race_time, race_distance))
-    winners = 0
-    for hold_time in range(0, big_race.time):
-        new_record = run_race(big_race.time, big_race.distance, hold_time)
-        winners += 1 if new_record > 0 else 0
-    ic(f'Part 2: {winners}')
+    product = 0
+    for i, player_hand in enumerate(reversed(ranked_game)):
+        product += ic((i + 1) * player_hand.bid_amount)
+    return product
 
 
 if __name__ == '__main__':
-    # data = get_data(day=7, year=2023).splitlines()
-    data = ['Time:      7  15   30',
-            'Distance:  9  40  200']
-    part1_solve(data)
-    part2_solve(data)
+    data = get_data(day=7, year=2023).splitlines()
+    # data = ['32T3K 765',
+    #         'T55J5 684',
+    #         'KK677 28',
+    #         'KTJJT 220',
+    #         'QQQJA 483']
+    ic(f'Part 1: {part1_solve(data)}')
+    # part2_solve(data)
