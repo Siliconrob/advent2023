27ff38856f2a1ed3e58ea3399722bb9acf1dcf89
Robin Michael <Siliconrob@siliconheaven.net>
2023-12-08 01:02:31 -0600

    Part 2 done find the paths individually and then multiply out a least common multiplier
    Because a 13 quintillion is super high

--- day8.py
+++ day8.py
@@ -1,3 +1,5 @@
+import math
+
 from aocd import get_data
 from collections import Counter, defaultdict
 from dataclasses import dataclass
@@ -32,24 +34,28 @@ def part1_solve(directions: str, start_id: str, end_id: str, paths: dict) -> int
 
 
 def part2_solve(directions: str, ends_with_start_node: str, ends_with_target_node: str, paths: dict) -> int:
-    ghost_paths = {}
+    ghost_path_starts = []
     for k, v in paths.items():
         if k.endswith(ends_with_start_node):
-            ghost_paths[k] = paths[k]
+            ghost_path_starts.append(paths[k])
 
-    total_steps = 0
-    for next_direction in cycle(directions):
-        total_steps += 1
-        completed_paths = sum([1 if cv.id.endswith(ends_with_target_node) else 0 for ck, cv in ghost_paths.items()])
-        if completed_paths == len(ghost_paths):
-            break
-        if next_direction == 'L':
-            for ck, cv in ghost_paths.items():
-                ghost_paths[ck] = paths[cv.left_node_id]
-            continue
-        for ck, cv in ghost_paths.items():
-            ghost_paths[ck] = paths[cv.right_node_id]
-    return total_steps - 1
+    ic(ghost_path_starts)
+    path_lengths = []
+    for ghost_path_start in ghost_path_starts:
+        current_node = ic(paths[ghost_path_start.id])
+        total_steps = 0
+        for next_direction in cycle(directions):
+            if current_node.id.endswith(ends_with_target_node):
+                break
+            if next_direction == 'L':
+                current_node = paths[current_node.left_node_id]
+                total_steps += 1
+                continue
+            current_node = paths[current_node.right_node_id]
+            total_steps += 1
+        path_lengths.append(total_steps)
+    complete_path_length = math.lcm(*path_lengths)
+    return complete_path_length
 
 
 def get_inputs(input_lines: []) -> (str, {}):
@@ -88,7 +94,7 @@ if __name__ == '__main__':
     #         '22Z = (22B, 22B)',
     #         'XXX = (XXX, XXX)']
 
-    # directions, part1_segments = ic(get_inputs(data))
-    # ic(f'Part 1: {part1_solve(directions, 'AAA', 'ZZZ', part1_segments)}')
+    directions, part1_segments = ic(get_inputs(data))
+    ic(f'Part 1: {part1_solve(directions, 'AAA', 'ZZZ', part1_segments)}')
     directions, part2_segments = ic(get_inputs(data))
     ic(f'Part 2: {part2_solve(directions, 'A', 'Z', part2_segments)}')
