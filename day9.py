13a97d0afd38f008d2bba84c0340c97c444fcac8
Robin Michael <Siliconrob@siliconheaven.net>
2023-12-09 00:03:26 -0600

    Needs recursion tried to write row, then looked up numpy diff
    Numpy does this already recursively and faster

--- day9.py
+++ day9.py
@@ -1,100 +1,37 @@
-import math
-
+from collections import deque
+import numpy as np
 from aocd import get_data
-from collections import Counter, defaultdict
-from dataclasses import dataclass
-from parse import parse
 from icecream import ic
-import networkx as nx
-from itertools import cycle
 
 
-@dataclass
-class PathSegment:
-    id: str
-    left_node_id: str
-    right_node_id: str
-
-
-def part1_solve(directions: str, start_id: str, end_id: str, paths: dict) -> int:
-    target = ic(paths[end_id])
-    current_node = ic(paths[start_id])
-
-    total_steps = 0
-    for next_direction in cycle(directions):
-        if current_node.id == target.id:
-            break
-        if next_direction == 'L':
-            current_node = paths[current_node.left_node_id]
-            total_steps += 1
-            continue
-        current_node = paths[current_node.right_node_id]
-        total_steps += 1
-    return total_steps
-
-
-def part2_solve(directions: str, ends_with_start_node: str, ends_with_target_node: str, paths: dict) -> int:
-    ghost_path_starts = []
-    for k, v in paths.items():
-        if k.endswith(ends_with_start_node):
-            ghost_path_starts.append(paths[k])
-
-    ic(ghost_path_starts)
-    path_lengths = []
-    for ghost_path_start in ghost_path_starts:
-        current_node = ic(paths[ghost_path_start.id])
-        total_steps = 0
-        for next_direction in cycle(directions):
-            if current_node.id.endswith(ends_with_target_node):
-                break
-            if next_direction == 'L':
-                current_node = paths[current_node.left_node_id]
-                total_steps += 1
-                continue
-            current_node = paths[current_node.right_node_id]
-            total_steps += 1
-        path_lengths.append(total_steps)
-    complete_path_length = math.lcm(*path_lengths)
-    return complete_path_length
-
-
-def get_inputs(input_lines: []) -> (str, {}):
-    directions = ic(input_lines[0])
-    path_segments = {}
-    for path_segment in input_lines[2:]:
-        id, left_node, right_node = parse('{} = ({}, {})', path_segment)
-        path_segments[id] = PathSegment(id, left_node, right_node)
-    return directions, path_segments
+#  This doesn't work right, but problem requires recursion
+def calc_value(current_stack: deque, current_number: int, current_diff: int) -> int:
+    if len(current_stack) == 0:
+        return current_diff
+    next_left = current_stack.pop()
+    current_diff = current_number - next_left
+    if current_diff != 0:
+        return ic(current_diff - calc_value(current_stack, next_left, current_diff))
+    return current_diff
 
 
 if __name__ == '__main__':
-    #data = get_data(day=9, year=2023).splitlines()
-    data = ['RL',
-            '',
-            'AAA = (BBB, CCC)',
-            'BBB = (DDD, EEE)',
-            'CCC = (ZZZ, GGG)',
-            'DDD = (DDD, DDD)',
-            'EEE = (EEE, EEE)',
-            'GGG = (GGG, GGG)',
-            'ZZZ = (ZZZ, ZZZ)']
-    # data = ['LLR',
-    #         '',
-    #         'AAA = (BBB, BBB)',
-    #         'BBB = (AAA, ZZZ)',
-    #         'ZZZ = (ZZZ, ZZZ)']
-    # data = ['LR',
-    #         '',
-    #         '11A = (11B, XXX)',
-    #         '11B = (XXX, 11Z)',
-    #         '11Z = (11B, XXX)',
-    #         '22A = (22B, XXX)',
-    #         '22B = (22C, 22C)',
-    #         '22C = (22Z, 22Z)',
-    #         '22Z = (22B, 22B)',
-    #         'XXX = (XXX, XXX)']
+    data = get_data(day=9, year=2023).splitlines()
+    # data = ['0 3 6 9 12 15',
+    #         '1 3 6 10 15 21',
+    #         '10 13 16 21 30 45']
 
-    directions, part1_segments = ic(get_inputs(data))
-    ic(f'Part 1: {part1_solve(directions, 'AAA', 'ZZZ', part1_segments)}')
-    directions, part2_segments = ic(get_inputs(data))
-    ic(f'Part 2: {part2_solve(directions, 'A', 'Z', part2_segments)}')
+    current_sums = []
+    for input_line in data:
+        current_line = [int(number.strip()) for number in input_line.split(" ")]
+        results = []
+        # https://numpy.org/doc/stable/reference/generated/numpy.diff.html
+        for current_number in range(len(current_line)):
+            new_row = np.diff(current_line, n=current_number)
+            results.append(new_row[-1])
+        current_sums.append(ic(sum(results)))
+    ic(f'Part 1: {sum(current_sums)}')
+    #     current_number = ic(current_line.pop())
+    #     #current_sums.append(ic(current_number + calc_value(current_line, current_number, 0)))
+    #     current_sums.append(ic(calc_value(current_line, current_number, 0)))
+    # ic(current_sums)
